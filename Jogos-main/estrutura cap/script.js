4746c20c511b3e9040cec539522395f34edaa581
Guilherme Gomes <134429767+GuiGomesD@users.noreply.github.com>
2024-10-05 18:44:43 -0300

    Add files via upload

--- Jogos-main/estrutura cap/script.js
+++ Jogos-main/estrutura cap/script.js
@@ -1,8 +1,8 @@
 function myFunction(x) {
     x.classList.toggle("change");
-  }
+}
 
-  let currentSlide = 0;
+let currentSlide = 0;
 
 const slides = [
     {
@@ -14,19 +14,23 @@ const slides = [
         text: 'Aqui está a segunda parte do capítulo 1.',
     }
 ];
-
+  
 function changeSlide(direction) {
     // Atualiza o slide atual
     currentSlide += direction;
-
+  
     // Verifica se o índice está fora dos limites e faz um loop
     if (currentSlide < 0) {
         currentSlide = slides.length - 1;
     } else if (currentSlide >= slides.length) {
         currentSlide = 0;
     }
-
-    // Altera a imagem e o texto do balão
-    document.getElementById('planet-image').src = slides[currentSlide].image;
+  
+    // Altera o background da div e o texto do balão
+    document.getElementById('planet-box').style.backgroundImage = `url(${slides[currentSlide].image})`;
     document.getElementById('bubble-text').innerHTML = `<p>${slides[currentSlide].text}</p>`;
 }
+  
+// Inicializa o primeiro slide como background
+document.getElementById('planet-box').style.backgroundImage = `url(${slides[0].image})`;
+  
