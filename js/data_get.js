cf7998739e3223338a391458300b06f41c76035b
Djallen Fabricio <djallenfab@MacBook-Air-de-Angeline.local>
2024-10-06 14:56:19 -0300

    Teste de carregamento de nomes no hmtl com base nno json

--- js/data_get.js
+++ js/data_get.js
@@ -1,1 +1,8 @@
 fetch("db/database.json")
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("pl_name").innerText = data.pl_name[2]
+    })
+    .catch(error => {
+        document.getElementById("response").innerText = "Falha ao pegar json"
+    })
